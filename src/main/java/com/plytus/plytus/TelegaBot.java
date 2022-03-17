72fe6b0c3b7519443fefb525c181eba1416c597e
doverchivost <doverchivostus@gmail.com>
2022-03-17 18:27:26 +0500

    Подготовить код для дальнейшей разработки

--- src/main/java/com/plytus/plytus/TelegaBot.java
+++ src/main/java/com/plytus/plytus/TelegaBot.java
@@ -2,12 +2,21 @@ package com.plytus.plytus;
 
 import com.pengrad.telegrambot.TelegramBot;
 import com.pengrad.telegrambot.UpdatesListener;
+import com.pengrad.telegrambot.model.Document;
+//import com.pengrad.telegrambot.model.File;
+import com.pengrad.telegrambot.model.MessageEntity;
 import com.pengrad.telegrambot.model.Update;
+import com.pengrad.telegrambot.model.request.ParseMode;
+import com.pengrad.telegrambot.request.SendDocument;
 import com.pengrad.telegrambot.request.SendMessage;
 import com.pengrad.telegrambot.response.SendResponse;
 
+import java.io.*;
+import java.util.Locale;
+
 public class TelegaBot {
     static TelegramBot bot;
+    static final File exampleCSV = new File("src/main/java/example.csv");
 
     public static void run() {
         String telegram_token = System.getenv("plytus_bot_token");
@@ -18,8 +27,33 @@ public class TelegaBot {
             for (Update update : updates) {
                 long chatId = update.message().chat().id();
                 String message = update.message().text();
-                String answer = TelegramMessageHandler.answer(chatId, message);
-                SendResponse response = bot.execute(new SendMessage(chatId, answer));
+                String answer = "";
+                //System.out.println(message);
+                if (!message.isEmpty()) {
+                    answer = TelegramMessageHandler.answer(chatId, message);
+                    if (message.equals("/add_from_csv")) {
+                        try {
+                            BufferedInputStream bis = new BufferedInputStream(new FileInputStream(exampleCSV));
+                            byte[] file = bis.readAllBytes();
+                            bot.execute(new SendDocument(chatId, file).caption("example.csv").fileName("example.csv"));
+
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+                else if (update.message().document() != null) {
+                    Document doc = update.message().document();
+                    String ext = doc.fileName().split("\\.")[doc.fileName().split("\\.").length - 1];
+                    answer = "ты отправил мне файл с расширением " + ext;
+                    if (ext.equalsIgnoreCase("csv")) {
+                        //обработать csv файл
+                    }
+                }
+                else {
+                    answer = "Ты отправил мне что-то не то :(";
+                }
+                bot.execute(new SendMessage(chatId, answer).parseMode(ParseMode.Markdown));
             }
             return UpdatesListener.CONFIRMED_UPDATES_ALL;
         });
--- src/main/java/com/plytus/plytus/TelegramMessageHandler.java
+++ src/main/java/com/plytus/plytus/TelegramMessageHandler.java
@@ -5,7 +5,10 @@ import com.plytus.plytus.services.ExpenseService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 
+import java.time.LocalDateTime;
 import java.util.Arrays;
+import java.util.Date;
+import java.util.Locale;
 import java.util.regex.Pattern;
 
 @Controller
@@ -19,15 +22,106 @@ public class TelegramMessageHandler {
     }
 
     public static String answer(long chatId, String message) {
-        if (message.equals("/start")) return "привет";
+        String decimalWithDotPattern = "([0-9]*)\\.([0-9]*)";
+        String decimalWithComaPattern = "([0-9]*),([0-9]*)";
+        String answer = "";
+        switch (message) {
+            case "/start":
+                answer = "привет";
+                break;
+            case "/add_expense":
+                answer = "Чтобы добавить трату, пришлите боту сообщение в формате:\n" +
+                        "   *Название траты*\n" +
+                        "   *Цена*\n" +
+                        "   *Категория*\n" +
+                        "Можно не указывать категорию, тогда она автоматически станет равной названию траты. \n" +
+                        "Например:\n" +
+                        "   *Карандаш*\n" +
+                        "   *13.50*\n" +
+                        "   *Канцелярия*";
+                break;
+            case "/delete_expense":
+                answer = "Чтобы удалить трату отправьте боту сообщение со словом «удалить» или «delete» и номером " +
+                        "траты. Номер траты можно узнать по команде \n`list of expenses` \n" +
+                        "Например: \n" +
+                        "   *удалить 1234*";
+                break;
+            case "/change_category":
+                answer = "Чтобы изменить категорию у определённой траты, отправьте боту сообщение со словом " +
+                        "«категория» или «category», номером траты и новым названием категории.\n" +
+                        "Например:\n" +
+                        "   *Категория 634 доставка еды*";
+                break;
+            case "/list_of_expenses":
+                answer = "я выведу список всех трат";
+                break;
+            case "/list_of_categories":
+                answer = "я выведу список категорий трат за месяц";
+                break;
+            case "/list_with_percentage":
+                answer = "я выведу список категорий трат за месяц в процентах";
+                break;
+            case "/add_from_csv":
+                answer = "Файл-пример в формате csv";
+                break;
+        }
 
+        if (answer.length() > 2) return answer;
+
+        String command = message.split(" ")[0].toLowerCase();
+        if (command.equals("удалить") || command.equals("delete")) {
+            int expenseIdToDelete = Integer.parseInt(message.split(" ")[1]);
+            answer = "БУДЕТ удалена трата с id = " + expenseIdToDelete;
+            //return answer;
+        }
+        else if (command.equals("категория") || command.equals("category")) {
+            int expenseId = Integer.parseInt(message.split(" ")[1]);
+            String newCategoryName = message.split(" ")[2].toLowerCase();
+            //проверить есть ли у юзера такая категория
+            //если да не создавая новую изменить внешний ключ на категорию в сущности
+            //если нет добавить в Категории новую категорию, изменить саму трату
+            answer = "у траты с id = " + expenseId + " будет категория ~" + newCategoryName + "~";
+            //return answer;
+        }
+        else if (message.split("\n").length > 1) {
+            if (Character.isDigit(message.split("\n")[1].charAt(0))) {
+                String[] msg = message.split("\n");
+                String expenseName = msg[0].toLowerCase().trim();
+                Date expenseDate = new Date();
+
+                String msgPrice = msg[1].trim();
+                double expensePrice = 0;
+                if (Pattern.matches(decimalWithDotPattern, msgPrice))
+                    expensePrice = Double.parseDouble(msgPrice);
+                else if (Pattern.matches(decimalWithComaPattern, msgPrice))
+                    expensePrice = Double.parseDouble(msgPrice.replace(",", "."));
+
+                String expenseCategory = "";
+                if (msg.length >= 3)
+                    expenseCategory = msg[2].toLowerCase();
+                else
+                    expenseCategory = expenseName;
+
+                Expense expense = new Expense(expenseName, expenseDate, expensePrice);
+                Long expenseId = expenseService.saveNewExpense(expense).getId();
+                answer = "Добавлена трата с \n" +
+                        "id = ~" + expenseId + "~\nname = ~" + expenseName + "~\n" +
+                        "price = ~" + expensePrice + "~\ndate = " + expenseDate + "~";
+            }
+        }
+        else {
+            answer = "что-то пошло не так :(";
+        }
+
+        return answer;
+
+        /*
         //пока без проверки на сообщения, т.е. только добавления трат "вкусное мороженое 15"
         String[] msg = message.split(" ");
         String expenseName = null;
         double expensePrice = 0;
 
-        String decimalWithDotPattern = "([0-9]*)\\.([0-9]*)";
-        String decimalWithComaPattern = "([0-9]*),([0-9]*)";
+
 
         for (int i = msg.length - 1; i > 0; i--){
             String msgPart = msg[i];
@@ -47,6 +141,6 @@ public class TelegramMessageHandler {
         Expense expense = new Expense(expenseName, expensePrice);
         Long idOfAddedExpense = expenseService.saveNewExpense(expense).getId();
         return "трата добавлена с id = " + idOfAddedExpense +
-                "\n трата: ~" + expenseName + "~ \n ее цена: ~" + expensePrice + "~";
+                "\n трата: ~" + expenseName + "~ \n ее цена: ~" + expensePrice + "~";*/
     }
 }
