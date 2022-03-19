c9d54c6b60a16c5fa2bebe660f7301a038dc2d1f
doverchivost <doverchivostus@gmail.com>
2022-03-19 16:33:12 +0500

    Реализовать  вывод списка категорий трат в процентах

--- src/main/java/com/plytus/plytus/TelegramMessageHandler.java
+++ src/main/java/com/plytus/plytus/TelegramMessageHandler.java
@@ -11,6 +11,7 @@ import org.springframework.stereotype.Controller;
 
 import java.util.*;
 import java.util.regex.Pattern;
+import java.util.stream.Collectors;
 
 @Controller
 public class TelegramMessageHandler {
@@ -68,7 +69,8 @@ public class TelegramMessageHandler {
                 answer = monthCategoryMessage(getAllUserExpensesForCurrentMonth(expenseUser));
                 break;
             case "/list_with_percentage":
-                answer = "я выведу список категорий трат за месяц в процентах";
+                //answer = "я выведу список категорий трат за месяц в процентах";
+                answer = monthCategoryPercentMessage(getAllUserExpensesForCurrentMonth(expenseUser));
                 break;
             case "/add_from_csv":
                 answer = "Файл-пример в формате csv";
@@ -79,50 +81,55 @@ public class TelegramMessageHandler {
 
         String command = message.split(" ")[0].toLowerCase();
         if (command.equals("удалить") || command.equals("delete")) {
-            long expenseIdToDelete = Long.parseLong(message.split(" ")[1]);
-            boolean expenseExist = checkExpenseExist(expenseIdToDelete);
-            if (expenseExist) {
-                Expense expenseToDelete = expenseService.getExpenseById(expenseIdToDelete);
-                if (expenseToDelete.getOwner().getId() == expenseUser.getId()) {
-                    expenseUser.getExpenses().remove(expenseToDelete);
-                    expenseToDelete.getCategory().getExpenses().remove(expenseToDelete);
-                    expenseService.deleteExpense(expenseToDelete);
-                    answer = "Удалена трата с id = " + expenseIdToDelete;
-                }
-                else {
-                    answer = "Это не ваша трата! Вы не можете ее удалить.";
-                }
-            }
-            else {
-                answer = "Такой траты не существует!";
-            }
-        }
-        else if (command.equals("категория") || command.equals("category")) {
-            String[] msg = message.split(" ");
-            long expenseId = Long.parseLong(msg[1]);
-            if (msg.length > 2) {
-                String[] name = Arrays.copyOfRange(msg, 2, msg.length);
-                String newCategoryName =String.join(" ", name);
-                Category newCategory = checkCategory(expenseUser, newCategoryName);
-
-                boolean expenseExist = checkExpenseExist(expenseId);
+            try {
+                long expenseIdToDelete = Long.parseLong(message.split(" ")[1]);
+                boolean expenseExist = checkExpenseExist(expenseIdToDelete);
                 if (expenseExist) {
-                    Expense expense = expenseService.getExpenseById(expenseId);
-                    if (expense.getOwner().getId() == expenseUser.getId()) {
-                        Expense expenseToChange = expenseService.getExpenseById(expenseId);
-                        expenseToChange.setCategory(newCategory);
-                        expenseService.saveNewExpense(expenseToChange);
-                        answer = "У траты с id = ~" + expenseToChange.getId() + "~ теперь \n" +
-                                "категория = ~" + expenseToChange.getCategory().getName() + "~";
+                    Expense expenseToDelete = expenseService.getExpenseById(expenseIdToDelete);
+                    if (expenseToDelete.getOwner().getId() == expenseUser.getId()) {
+                        //expenseUser.getExpenses().remove(expenseToDelete);
+                        //expenseToDelete.getCategory().getExpenses().remove(expenseToDelete);
+                        expenseService.deleteExpense(expenseToDelete);
+                        answer = "Удалена трата с id = " + expenseIdToDelete;
                     } else {
-                        answer = "Это не ваша трата! Вы не можете менять ее категорию.";
+                        answer = "Это не ваша трата! Вы не можете ее удалить.";
                     }
                 } else {
                     answer = "Такой траты не существует!";
                 }
+            } catch (NumberFormatException e) {
+                answer = "id должен быть цифрой";
             }
-            else {
-                answer = "Неправильная команда";
+        }
+        else if (command.equals("категория") || command.equals("category")) {
+            String[] msg = message.split(" ");
+            try {
+                long expenseId = Long.parseLong(msg[1]);
+                if (msg.length > 2) {
+                    String[] name = Arrays.copyOfRange(msg, 2, msg.length);
+                    String newCategoryName = String.join(" ", name);
+                    Category newCategory = checkCategory(expenseUser, newCategoryName);
+
+                    boolean expenseExist = checkExpenseExist(expenseId);
+                    if (expenseExist) {
+                        Expense expense = expenseService.getExpenseById(expenseId);
+                        if (expense.getOwner().getId() == expenseUser.getId()) {
+                            Expense expenseToChange = expenseService.getExpenseById(expenseId);
+                            expenseToChange.setCategory(newCategory);
+                            expenseService.saveNewExpense(expenseToChange);
+                            answer = "У траты с id = ~" + expenseToChange.getId() + "~ теперь \n" +
+                                    "категория = ~" + expenseToChange.getCategory().getName() + "~";
+                        } else {
+                            answer = "Это не ваша трата! Вы не можете менять ее категорию.";
+                        }
+                    } else {
+                        answer = "Такой траты не существует!";
+                    }
+                } else {
+                    answer = "Неправильная команда";
+                }
+            } catch (NumberFormatException e) {
+                answer = "id должен быть цифрой";
             }
         }
         else if (message.split("\n").length > 1) {
@@ -218,7 +225,7 @@ public class TelegramMessageHandler {
     }
 
     private static String monthExpenseMessage(Set<Expense> expenseSet) {
-        String answer = "id) название (категория) : цена\n\n";
+        String answer = "id) название (категория) : сумма\n\n";
         double priceTotal = 0;
         for (Expense expense : expenseSet) {
             answer += String.format("%1$s) %2$s (%3$s) : %4$.2f\n",
@@ -231,18 +238,7 @@ public class TelegramMessageHandler {
 
     private static String monthCategoryMessage(Set<Expense> expenseSet) {
         String answer = "категория - сумма\n\n";
-        HashMap<String, Double> categoryPrice = new HashMap<String, Double>();
-        for (Expense expense : expenseSet) {
-            String expenseCategory = expense.getCategory().getName();
-            if (categoryPrice.containsKey(expenseCategory)) {
-                double price = expense.getPrice() + categoryPrice.get(expenseCategory);
-                categoryPrice.replace(expenseCategory, price);
-            }
-            else {
-                categoryPrice.put(expenseCategory, expense.getPrice());
-            }
-        }
-
+        Map<String, Double> categoryPrice = getCategoryPriceMap(expenseSet);
         double priceTotal = 0;
         Set<String> keys = categoryPrice.keySet();
         for (String key : keys) {
@@ -253,4 +249,59 @@ public class TelegramMessageHandler {
         answer += String.format("\nСумма всех трат за месяц: %.2f", priceTotal);
         return answer;
     }
+
+    private static String monthCategoryPercentMessage(Set<Expense> expenseSet) {
+        String answer = "% категория (сумма)\n\n";
+        Map<String, Double> categoryPrice = getCategoryPriceMap(expenseSet);
+        double priceTotal = 0;
+        for(Expense expense : expenseSet)
+            priceTotal += expense.getPrice();
+
+        Map<String, Double> sortedByPrice = sort(categoryPrice, false);
+        for (String category : sortedByPrice.keySet()) {
+            double price = sortedByPrice.get(category);
+            double percentPrice = price / priceTotal * 100;
+            //answer += percentPrice + "% " + category + " (" + price + ")\n";
+            answer += String.format("%.2f%% %s (%.2f)\n", percentPrice, category, price);
+        }
+
+        answer += String.format("\nСумма всех трат за месяц: %.2f", priceTotal);
+        return answer;
+    }
+
+    private static Map<String, Double> getCategoryPriceMap (Set<Expense> expenseSet) {
+        Map<String, Double> categoryPrice = new HashMap<String, Double>();
+        for (Expense expense : expenseSet) {
+            String expenseCategory = expense.getCategory().getName();
+            if (categoryPrice.containsKey(expenseCategory)) {
+                double price = expense.getPrice() + categoryPrice.get(expenseCategory);
+                categoryPrice.replace(expenseCategory, price);
+            }
+            else {
+                categoryPrice.put(expenseCategory, expense.getPrice());
+            }
+        }
+        return categoryPrice;
+    }
+
+    private static <K extends Comparable<K>, V extends Comparable<V>> Map<K, V> sort(
+            final Map<K, V> unsorted,
+            final boolean order) {
+        final var list = new LinkedList<>(unsorted.entrySet());
+
+        list.sort((o1, o2) -> order
+                ? o1.getValue().compareTo(o2.getValue()) == 0
+                ? o1.getKey().compareTo(o2.getKey())
+                : o1.getValue().compareTo(o2.getValue())
+                : o2.getValue().compareTo(o1.getValue()) == 0
+                ? o2.getKey().compareTo(o1.getKey())
+                : o2.getValue().compareTo(o1.getValue()));
+        return list.stream().collect(
+                Collectors.toMap(
+                        Map.Entry::getKey, Map.Entry::getValue, (a, b) -> b, LinkedHashMap::new
+                )
+        );
+    }
+
+
 }
