e805158c94bb458ee114fd198650b73fcd42a4ee
doverchivost <doverchivostus@gmail.com>
2022-05-02 01:19:00 +0500

    Обработать ошибку при вводе символа "*"

--- src/main/java/com/plytus/plytus/TelegaBot.java
+++ src/main/java/com/plytus/plytus/TelegaBot.java
@@ -44,10 +44,15 @@ public class TelegaBot {
                 String message = update.message().text();
                 String answer = "";
                 if (message != null) {
-                    message = message.trim();
-                    answer = TelegramMessageHandler.answer(chatId, message);
-                    if (message.equals("/add_from_csv")) {
-                        bot.execute(new SendDocument(chatId, exampleCSV).caption("example.csv").fileName("example.csv"));
+                    if (message.contains("*")) {
+                        answer = "Недопустимый символ _*_";
+                    }
+                    else {
+                        message = message.trim();
+                        answer = TelegramMessageHandler.answer(chatId, message);
+                        if (message.equals("/add_from_csv")) {
+                            bot.execute(new SendDocument(chatId, exampleCSV).caption("example.csv").fileName("example.csv"));
+                        }
                     }
                 }
                 else if (update.message().document() != null) {
